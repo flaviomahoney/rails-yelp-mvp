04991ac5edc7ecfe5b4fd4eccedfadd6f408c466
flaviomahoney <flavio@mahoneyt.com>
2020-05-27 14:29:40 -0300

    Adding exercise two

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  Restaurant.create(
+    name: "Amalerinho",
+    address: "Piratininga",
+    phone_number: "+55 21 35667892",
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  Restaurant.create(
+    name: "Brazuca",
+    address: "Marina da Gloria",
+    phone_number: "+55 21 38952614",
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  Restaurant.create(
+    name: "Carretão",
+    address: "Copacabana",
+    phone_number: "+55 21 77884426",
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  Restaurant.create(
+    name: "Porcão",
+    address: "Urca",
+    phone_number: "+55 21 24568875",
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  Restaurant.create(
+    name: "Paludo",
+    address: "São Francisco",
+    phone_number: "+55 21 14526472",
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
